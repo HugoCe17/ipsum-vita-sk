8c67add96c51849ca12dea2b0bcb7e3bd9746413
HugoCe17 <hugoce17@gmail.com>
2014-07-07 05:59:19 -0400

    Various CSS tweaks/fixes to further support different devices

---
+++ app/scripts/RenderPDF.js
@@ -0,0 +1,26 @@
+
+    //Disable workers to avoid cross-oriding issue
+    PDFJS.disableWorker = true;
+    PDFJS.getDocument('PDF/Resume2014.pdf').then(function getPDFResume(pdf) {
+        //
+        // Fetch first Page
+        //
+        pdf.getPage(1).then(function getPageResume(page) {
+            var scale = 1;
+            var viewport = page.getViewport(scale);
+            //
+            // Prepare canvast using PDF page dimensions
+            //
+            var canvas = document.getElementById('pdf-canvas');
+            var context = canvas.getContext('2d');
+            canvas.height = viewport.height;
+            canvas.width = viewport.width;
+            //
+            // Render PDG page into canvas context
+            //
+            page.render({
+                canvasContext: context,
+                viewport: viewport
+            });
+        });
+    });
