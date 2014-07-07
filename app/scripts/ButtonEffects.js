32545f5552fab0ba42a230285005999e9533992b
HugoCe17 <hugoce17@gmail.com>
2014-07-07 19:28:59 -0400

    CSS Tweaks/SoundCloud Widget Tweaks

---
+++ app/scripts/ButtonEffects.js
@@ -0,0 +1,17 @@
+    $("#resume-btn").click(function() {
+        $(".about-me").hide("fast");
+        $(".blading-adventures").hide("fast");
+        $(".resume").fadeIn("fast");
+    });
+
+    $("#about-me-btn").click(function() {
+        $(".resume").hide("fast");
+        $(".blading-adventures").hide("fast");
+        $(".about-me").fadeIn("fast");
+    });
+
+    $("#skate-sesh-btn").click(function() {
+        $(".resume").hide("fast");
+        $(".about-me").hide("fast");
+        $(".blading-adventures").fadeIn("fast");
+    });
