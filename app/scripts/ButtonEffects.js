2e69ae4eb64ed79bbebf77652045485514865b1b
HugoCe17 <hugoce17@gmail.com>
2014-07-08 03:12:50 -0400

    Optimized Javascript

--- app/scripts/ButtonEffects.js
+++ app/scripts/ButtonEffects.js
@@ -1,17 +1,19 @@
-    $("#resume-btn").click(function() {
-        $(".about-me").hide("fast");
-        $(".blading-adventures").hide("fast");
+    'use strict';
+
+    $('#resume-btn').click(function() {
+        $('.about-me').hide('fast');
+        $('.blading-adventures').hide('fast');
         $(".resume").fadeIn("fast");
     });
 
     $("#about-me-btn").click(function() {
-        $(".resume").hide("fast");
-        $(".blading-adventures").hide("fast");
-        $(".about-me").fadeIn("fast");
+        $('.resume').hide('fast');
+        $('.blading-adventures').hide('fast');
+        $('.about-me').fadeIn('fast');
     });
 
-    $("#skate-sesh-btn").click(function() {
-        $(".resume").hide("fast");
-        $(".about-me").hide("fast");
-        $(".blading-adventures").fadeIn("fast");
+    $('#skate-sesh-btn').click(function() {
+        $('.resume').hide('fast');
+        $('.about-me').hide('fast');
+        $('.blading-adventures').fadeIn('fast');
     });
--- app/scripts/RenderPDF.js
+++ app/scripts/RenderPDF.js
@@ -1,26 +1,27 @@
+"use strict";
 
-    //Disable workers to avoid cross-oriding issue
-    PDFJS.disableWorker = true;
-    PDFJS.getDocument('PDF/Resume2014.pdf').then(function getPDFResume(pdf) {
+//Disable workers to avoid cross-oriding issue
+PDFJS.disableWorker = true;
+PDFJS.getDocument('PDF/Resume2014.pdf').then(function getPDFResume(pdf) {
+    //
+    // Fetch first Page
+    //
+    pdf.getPage(1).then(function getPageResume(page) {
+        var scale = 1;
+        var viewport = page.getViewport(scale);
         //
-        // Fetch first Page
+        // Prepare canvast using PDF page dimensions
         //
-        pdf.getPage(1).then(function getPageResume(page) {
-            var scale = 1;
-            var viewport = page.getViewport(scale);
-            //
-            // Prepare canvast using PDF page dimensions
-            //
-            var canvas = document.getElementById('pdf-canvas');
-            var context = canvas.getContext('2d');
-            canvas.height = viewport.height;
-            canvas.width = viewport.width;
-            //
-            // Render PDG page into canvas context
-            //
-            page.render({
-                canvasContext: context,
-                viewport: viewport
-            });
+        var canvas = document.getElementById('pdf-canvas');
+        var context = canvas.getContext('2d');
+        canvas.height = viewport.height;
+        canvas.width = viewport.width;
+        //
+        // Render PDG page into canvas context
+        //
+        page.render({
+            canvasContext: context,
+            viewport: viewport
         });
     });
+});
